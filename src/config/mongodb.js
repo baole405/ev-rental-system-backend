66111ef9fb2430162e62c7c91ee269e1c9d0dde4
baole405 <baolttse184569@fpt.edu.vn>
2025-10-06 22:43:11 +0700

    chore: first commit

---
+++ src/config/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.ATLAS_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export default connectDB;
