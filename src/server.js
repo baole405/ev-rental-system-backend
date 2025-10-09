ca2e5c5a2d7d9d2344480b0e61d9e90c1e736f5b
baole405 <baolttse184569@fpt.edu.vn>
2025-10-09 09:10:51 +0700

    build: docker images and build

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
 
 import connectDB from "./config/mongodb.js";
+import bookingRoutes from "./routes/booking.routes.js";
+import handoverRoutes from "./routes/handover.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
+import rentalRoutes from "./routes/rental.routes.js";
+import stationRoutes from "./routes/station.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import userDocRoutes from "./routes/userDocument.routes.js";
 import vehicleRoutes from "./routes/vehicle.routes.js";
-import { seedVehicles } from "./seed/vehicle.seed.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
@@ -16,6 +22,13 @@ app.get("/", (req, res) => {
   res.json({ message: "EV Rental System API" });
 });
 
+app.use("/api/booking", bookingRoutes);
+app.use("/api/handovers", handoverRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/rentals", rentalRoutes);
+app.use("/api/stations", stationRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/userDocs", userDocRoutes);
 app.use("/api/vehicles", vehicleRoutes);
 
 app.use((err, req, res, next) => {
@@ -29,7 +42,6 @@ app.use((err, req, res, next) => {
 const startServer = async () => {
   try {
     await connectDB();
-    await seedVehicles();
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
