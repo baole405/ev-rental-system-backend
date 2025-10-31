a5ee8395a73a5baab5909b555b2cb2df337fcf9b
baole <tommydao2000@gmail.com>
2025-10-31 21:16:38 +0700

    Document vehicle specifications in Swagger

--- src/routes/brand.routes.js
+++ src/routes/brand.routes.js
@@ -5,12 +5,14 @@ import {
   createBrand,
   updateBrand,
   deleteBrand,
+  getBrandVehicleCount,
 } from "../controllers/brand.controller.js";
 
 const router = Router();
 
 router.get("/", listBrands);
 router.get("/:id", getBrand);
+router.get("/:id/vehicles/count", getBrandVehicleCount);
 router.post("/", createBrand);
 router.put("/:id", updateBrand);
 router.delete("/:id", deleteBrand);
